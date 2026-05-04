6dc75a06eb4c4f9c8ea8a603fe0b0bbb68173257
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-04 16:37:39 -0300

    feat: add a function to initialize dev controller and all endpoints

--- internal/devs/initializer.go
+++ internal/devs/initializer.go
@@ -1,11 +1,34 @@
 package devs
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"net/http"
 
-func InitializeDevController(db *sql.DB) *DevController {
+	"github.com/CriciumaDevJobs/backend/handlers"
+)
+
+func InitializeDevContext(db *sql.DB) {
+	controller := initializeDevController(db)
+	initializeDevEndpoints(controller)
+}
+
+func initializeDevController(db *sql.DB) *DevController {
 	repository := New(db)
 	usecase := NewDevUseCase(repository)
 	controller := NewDevController(usecase)
 
 	return controller
+}
+
+func initializeDevEndpoints(controller *DevController) {
+	http.HandleFunc("/devs", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			controller.CreateDev(context.Background(), w, r)
+
+		default:
+			handlers.ResponseWithHttpError(w, http.StatusMethodNotAllowed, "Method not Allowed")
+		}
+	})
 }
