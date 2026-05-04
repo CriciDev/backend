e51402ee208eea4316fa06c704568cbf523f6d61
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-04 16:09:23 -0300

    feat: add sql schema for devs table

---
+++ sql/schemas/devs.sql
@@ -0,0 +1,11 @@
+CREATE TABLE devs (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    skills TEXT[],
+    bio TEXT NOT NULL,
+    availability BOOLEAN NOT NULL DEFAULT true,
+    socials TEXT[],
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
