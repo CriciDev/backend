c6942f03ee1709a89837e4442d2678a109614e23
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-04 16:40:06 -0300

    feat: add default error message for longer passwords

--- handlers/dev_errors.go
+++ handlers/dev_errors.go
@@ -7,4 +7,5 @@ var (
 	ErrEmailNotEmpty     = errors.New("Campo Email deve ser preenchido!")
 	ErrEmailAlreadyInUse = errors.New("Email já está em uso")
 	ErrBioNotEmpty       = errors.New("Campo Biografia deve ser preenchido")
+	ErrPasswordToLong    = errors.New("O Campo de Senha não pode ultrapassar 72 caracteres")
 )
