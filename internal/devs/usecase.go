b51760131e8e7ced0c61917228b28c777f8ee45f
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-04 16:02:39 -0300

    feat: add validation if email is already in use and inject repository generated by sqlc

--- internal/devs/usecase.go
+++ internal/devs/usecase.go
@@ -2,13 +2,16 @@ package devs
 
 import (
 	"context"
+
+	"github.com/CriciumaDevJobs/backend/handlers"
+	"github.com/CriciumaDevJobs/backend/infra"
 )
 
 type DevUseCase struct {
-	Repository *DevRepository
+	Repository *Queries
 }
 
-func NewDevUseCase(devRepository *DevRepository) *DevUseCase {
+func NewDevUseCase(devRepository *Queries) *DevUseCase {
 	usecase := DevUseCase{
 		Repository: devRepository,
 	}
@@ -16,6 +19,51 @@ func NewDevUseCase(devRepository *DevRepository) *DevUseCase {
 	return &usecase
 }
 
-func (usecase *DevUseCase) CreateDev(ctx context.Context, dev *Dev) error {
-	return usecase.Repository.CreateDev(ctx, dev)
+func (usecase *DevUseCase) CreateDev(ctx context.Context, dev *Dev) (CreateDevRow, error) {
+
+	err := dev.validate()
+
+	if err != nil {
+		return CreateDevRow{}, err
+	}
+
+	_, err = usecase.Repository.FindDevByEmail(ctx, dev.Email)
+
+	if err == nil {
+		return CreateDevRow{}, handlers.ErrEmailAlreadyInUse
+	}
+
+	hashedPassword, err := infra.EncryptPassword(dev.Password)
+
+	if err != nil {
+		return CreateDevRow{}, err
+	}
+
+	var devParams = CreateDevParams{
+		Name:         dev.Name,
+		Email:        dev.Email,
+		Password:     hashedPassword,
+		Skills:       dev.Skills,
+		Bio:          dev.Bio,
+		Availability: dev.Availability,
+		Socials:      dev.Socials,
+	}
+
+	return usecase.Repository.CreateDev(ctx, devParams)
+}
+
+func (dev *Dev) validate() error {
+	if dev.Name == "" {
+		return handlers.ErrNameNotEmpty
+	}
+
+	if dev.Email == "" {
+		return handlers.ErrEmailNotEmpty
+	}
+
+	if dev.Bio == "" {
+		return handlers.ErrBioNotEmpty
+	}
+
+	return nil
 }
