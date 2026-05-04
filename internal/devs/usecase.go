b732dcd62b437e90409eecd14744a82842715b0e
juanc <juancarlosbatistazeferino@gmail.com>
2026-05-03 22:53:36 -0300

    feat: add dev usecase

--- internal/devs/usecase.go
+++ internal/devs/usecase.go
@@ -1,1 +1,21 @@
 package devs
+
+import (
+	"context"
+)
+
+type DevUseCase struct {
+	Repository *DevRepository
+}
+
+func NewDevUseCase(devRepository *DevRepository) *DevUseCase {
+	usecase := DevUseCase{
+		Repository: devRepository,
+	}
+
+	return &usecase
+}
+
+func (usecase *DevUseCase) CreateDev(ctx context.Context, dev *Dev) error {
+	return usecase.Repository.CreateDev(ctx, dev)
+}
