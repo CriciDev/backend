d3d0d1449190fe65baaf5a810f6ca750ea34c163
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-04 16:39:14 -0300

    feat: adds an error for passwords longer than 72 characters

--- infra/password_encoder.go
+++ infra/password_encoder.go
@@ -1,8 +1,19 @@
 package infra
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"unicode/utf8"
+
+	"github.com/CriciumaDevJobs/backend/handlers"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func EncryptPassword(password string) (string, error) {
+
+	//bcrypt encoder trunkates string with more then 72 characters
+	if utf8.RuneCountInString(password) > 72 {
+		return "", handlers.ErrPasswordToLong
+	}
+
 	hashedpassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
 	if err != nil {
