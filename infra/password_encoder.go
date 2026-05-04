1c9a05e29cedbc62959c1630760bfd9787eddecb
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-04 15:56:09 -0300

    feat: add password encoder function

---
+++ infra/password_encoder.go
@@ -0,0 +1,13 @@
+package infra
+
+import "golang.org/x/crypto/bcrypt"
+
+func EncryptPassword(password string) (string, error) {
+	hashedpassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedpassword), nil
+}
