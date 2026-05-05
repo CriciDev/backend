8f29df710c4a0135bbbd3e6b33576b2c427a68b0
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-05 14:36:43 -0300

    feat: add usecase for user authentication

--- internal/auth/usecase.go
+++ internal/auth/usecase.go
@@ -1,1 +1,51 @@
 package auth
+
+import (
+	"context"
+	"time"
+
+	"github.com/CriciumaDevJobs/backend/handlers"
+	"github.com/CriciumaDevJobs/backend/internal/devs"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthenticationUseCase struct {
+	DevUseCase *devs.DevUseCase
+}
+
+func NewAuthenticationUseCase(devUseCase *devs.DevUseCase) *AuthenticationUseCase {
+	auth := AuthenticationUseCase{
+		DevUseCase: devUseCase,
+	}
+
+	return &auth
+}
+
+func (usecase *AuthenticationUseCase) AuthenticateUser(ctx context.Context, email string, password string) (*AuthenticationResponse, *handlers.ErrorResponse) {
+	dev, err := usecase.DevUseCase.FindDevByEmail(ctx, email)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bcrypterr := bcrypt.CompareHashAndPassword([]byte(dev.Password), []byte(password))
+
+	if bcrypterr != nil {
+		return nil, handlers.ErrInvalidEmailOrPassword
+	}
+
+	expiration := time.Now().Add(time.Hour * 4)
+
+	token, err := GenerateJwtToken(dev, expiration)
+
+	if err != nil {
+		return nil, err
+	}
+
+	authResponse := AuthenticationResponse{
+		Token:     token,
+		ExpiresAt: expiration,
+	}
+
+	return &authResponse, nil
+}
