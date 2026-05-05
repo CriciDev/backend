16a78d53a8d8d3ba5ccdca0c6b7d37ff319a2578
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-05 16:16:56 -0300

    feat: add new query to check if email is already registered

--- sql/queries/devs.sql
+++ sql/queries/devs.sql
@@ -6,3 +6,6 @@ SELECT id, name, email, password FROM devs WHERE email = $1;
 
 -- name: FindDevByID :one
 SELECT name, email, skills, bio, availability, socials FROM devs WHERE id = $1;
+
+-- name: EmailAlreadyRegistered :one
+SELECT count(*) AS total FROM devs WHERE email = $1;
