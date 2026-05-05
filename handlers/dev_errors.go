0ac87ed36ba067d593c472cf372400ece997f0fc
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-05 16:15:52 -0300

    feat: add error for invalid email address

--- handlers/dev_errors.go
+++ handlers/dev_errors.go
@@ -7,6 +7,7 @@ import (
 var (
 	ErrNameNotEmpty           = NewError(http.StatusUnprocessableEntity, "Campo Nome deve ser preenchido!")
 	ErrEmailNotEmpty          = NewError(http.StatusUnprocessableEntity, "Campo Email deve ser preenchido!")
+	ErrEmailAddressNotValid   = NewError(http.StatusUnprocessableEntity, "Por favor, Insira um email válido")
 	ErrEmailAlreadyInUse      = NewError(http.StatusUnprocessableEntity, "Email já está em uso")
 	ErrBioNotEmpty            = NewError(http.StatusUnprocessableEntity, "Campo Biografia deve ser preenchido")
 	ErrPasswordToLong         = NewError(http.StatusUnprocessableEntity, "O Campo de Senha não pode ultrapassar 72 caracteres")
